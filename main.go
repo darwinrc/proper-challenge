469bba1c0c6ab9713e2f7c77c7937aee1b92c713
darwinrc <darwinrc@gmail.com>
2022-11-13 16:38:35 -0500

    Parallelize getImages

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ func main() {
 	)
 
 	var (
-		err     error
 		amount  = flag.Int("amount", 10, "amount")
 		threads = flag.Int("threads", 1, "threads")
 	)
@@ -29,26 +28,27 @@ func main() {
 		log.Fatal("threads should be between 1 and 5")
 	}
 
-	images, err := getImages(*amount, perPage)
-	if err != nil {
-		log.Fatal(err)
-	}
+	images := getImages(*amount, *threads, perPage)
 
-	if err := storeImages(images, *threads); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+	storeImages(images, *threads)
 
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
 // getImages returns a slice of images depending on the amount and items per page
-func getImages(amount, perPage int) (images []*file.File, err error) {
+// It executes using the number of concurrent goroutines limited by 'thread'
+func getImages(amount, threads, perPage int) (images []*file.File) {
 	baseUrl := "https://icanhas.cheezburger.com"
 
 	pages := amount / perPage
 	if amount%perPage != 0 {
 		pages += 1
 	}
+
+	wg := new(sync.WaitGroup)
+	ch := make(chan struct{}, threads)
 
 	for p := 1; p <= pages; p++ {
 		url := baseUrl
@@ -58,26 +58,36 @@ func getImages(amount, perPage int) (images []*file.File, err error) {
 		web := web.Web{
 			Url: url,
 		}
-		if err := web.FetchPage(); err != nil {
-			return nil, err
-		}
 
-		pageImages, err := web.GetImages(".mu-content-card", amount, p, perPage)
-		if err != nil {
-			return nil, err
-		}
-		images = append(images, pageImages...)
+		ch <- struct{}{}
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup, images *[]*file.File) {
+			defer wg.Done()
+			if err := web.FetchPage(); err != nil {
+				log.Fatal(err)
+			}
+
+			pageImages, err := web.GetImages(".mu-content-card", amount, p, perPage)
+			if err != nil {
+				log.Fatal(err)
+			}
+			*images = append(*images, pageImages...)
+			<-ch
+		}(wg, &images)
 	}
+
+	wg.Wait()
 
 	return
 }
 
 // storeImages gets the images stream data and saves them to local file system as files
 // It executes using the number of concurrent goroutines limited by 'thread'
-func storeImages(images []*file.File, threads int) error {
+func storeImages(images []*file.File, threads int) {
 	dir := "img/"
 	if err := file.MkDir(dir); err != nil {
-		return err
+		log.Fatal(err)
 	}
 
 	wg := new(sync.WaitGroup)
@@ -86,6 +96,7 @@ func storeImages(images []*file.File, threads int) error {
 	for _, img := range images {
 		ch <- struct{}{}
 		wg.Add(1)
+
 		go func(wg *sync.WaitGroup, img *file.File) {
 			defer wg.Done()
 			var err error
@@ -102,6 +113,4 @@ func storeImages(images []*file.File, threads int) error {
 		}(wg, img)
 	}
 	wg.Wait()
-
-	return nil
 }
